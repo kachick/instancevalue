ea14fa068518f849981a36e7d343d7d4085514b9
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-04-06 01:19:18 +0900

    first release

---
+++ example/example.rb
@@ -0,0 +1,22 @@
+$VERBOSE = true
+
+require_relative '../lib/instancevalue'
+
+class Person
+  include InstanceValue
+  
+  value_reader :birthday
+
+  def initialize(birthday)
+    val :birthday, birthday
+  end
+
+  def age
+    (Time.now - val(:birthday)) / (60 * 60 * 24 * 365)
+  end
+end
+
+person = Person.new Time.at 8888
+p person.birthday
+p person.age                                    #=> age at runtime
+#~ person.instance_eval{val :birthday, Time.now} #=> Exception
---
+++ lib/instancevalue.rb
@@ -0,0 +1,95 @@
+# Instance Value
+#   Set constant values for each instance.
+#   Copyright (C) 2012  Kenichi Kamiya
+
+# @example Overview
+#     class Person
+#       include InstanceValue
+#       
+#       value_reader :birthday
+#
+#       def initialize(birthday)
+#         val :birthday, birthday
+#       end
+#
+#       def age
+#         (Time.now - val(:birthday)) / (60 * 60 * 24 * 365)
+#       end
+#     end
+#     
+#     person = Person.new YOUR_BIRTHDAY
+#     person.birthday
+#     person.age                                    #=> age at runtime
+#     person.instance_eval{val :birthday, Time.now} #=> Exception
+module InstanceValue
+  VERSION = '0.0.1'.freeze
+
+  module Eigen
+    private
+    
+    def value_reader(name)
+      define_method name do
+        instance_value_get name
+      end
+    end
+  end
+
+  class << self
+    private
+    
+    def included(mod)
+      mod.extend Eigen
+    end
+  end
+
+  # @example
+  #     val(name)              #=> get
+  #     val(name, const_value) #=> set
+  def val(name, *values)
+    case values.length
+    when 0
+      instance_value_get name
+    when 1
+      instance_value_set name, values.first
+    else
+      raise ArgumentError, "wrong number of Argument #{values.length} for 1 or 2"
+    end
+  end
+
+  def instance_value_defined?(name)
+    unless name.instance_of?(Symbol) or name.respond_to?(:to_str)
+      raise TypeError
+    end
+    
+    if singleton_class.const_defined? :VALUES
+      singleton_class::VALUES.has_key? name.to_sym
+    else
+      singleton_class.const_set :VALUES, {}
+      false
+    end
+  end
+  
+  def instance_value_get(name)
+    instance_value_defined?(name) ? singleton_class::VALUES[name.to_sym] : nil
+  end
+  
+  def instance_value_set(name, value)
+    if instance_value_defined? name
+      raise "value #{} was already binded"
+    else
+      singleton_class::VALUES[name.to_sym] = value
+    end
+  end
+  
+  def instance_values
+    singleton_class::VALUES.keys
+  end
+  
+  def remove_instance_value(name)
+    if instance_value_defined? name
+      singleton_class::VALUES.delete_key name.to_sym
+    else
+      raise NameError
+    end
+  end
+end
